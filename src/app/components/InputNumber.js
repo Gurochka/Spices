f5c31fb425f904c7746ba374ac05def084fc501f
gurochka <natali.ananyeva@gmail.com>
2020-03-22 12:20:20 +0400

    add Icons components

--- src/app/components/InputNumber.js
+++ src/app/components/InputNumber.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './InputNumber.scss'
+import IconMinus from 'App/components/images/IconMinus.js'
+import IconPlus from 'App/components/images/IconPlus.js'
 
 class Order extends React.Component {
   onChangeAmount(diff){
@@ -20,11 +22,11 @@ class Order extends React.Component {
     return  (
       <div className="input-number">
         <div onClick={() => this.onChangeAmount(-diff)}>
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="minus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+          <IconMinus />
         </div>
         <input value={this.props.value} onChange={this.onInputHandler} type="text"/>
         <div onClick={() => this.onChangeAmount(diff)}>
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg>
+          <IconPlus />
         </div>
       </div>
     )
