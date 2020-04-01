51e4607b05bad5707c3694c4f5b91838f5d8ec39
gurochka <natali.ananyeva@gmail.com>
2020-04-01 08:49:22 +0400

    add new route Cart

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SpiceSetMaker from 'App/pages/SpiceSetMaker'
+import Cart from 'App/pages/Cart'
 
 function App() {
   return (
-    <SpiceSetMaker />
+    <Router>
+      <Route path="/" exact component={SpiceSetMaker} />
+      <Route path="/cart" component={Cart} />
+    </Router> 
   )
 }
 
---
+++ src/app/pages/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Cart extends React.Component {
+  state = {
+  }
+
+  render(){
+    return  (
+      <h1>Cart</h1>
+    )
+  }
+}
+export default Cart
